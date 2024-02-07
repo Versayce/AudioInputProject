d869447bc322d0b497ee282734b2b57c45b49249
Alex <acastro166@gmail.com>
2024-02-07 14:03:10 -0700

    reading audio devices properly

--- audio-test/src/app/InputSelection.tsx
+++ audio-test/src/app/InputSelection.tsx
@@ -1,12 +1,22 @@
 'use client'
 
 export default function InputSelection() {
-    function inputSelect() {
+    
+    const inputSelect = async () =>{
+        let stream = null;
+        try {
+            stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+            console.log("Got audio stream");
+        } catch (error) {
+            console.error("Error getting audio stream", error);
+        }
         console.log("Selecting audio source");
         navigator.mediaDevices.enumerateDevices().then((devices) => {
             console.log(devices);
         });
+        return stream
     }
+    
     return (
         <div>
             <button onClick={inputSelect}>Audio Source</button>
