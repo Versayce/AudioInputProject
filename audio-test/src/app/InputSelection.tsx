286a74288c8f69e5d01a199cf28b6a750079d27d
Alex <acastro166@gmail.com>
2024-02-27 04:07:51 -0700

    added device selection

--- audio-test/src/app/InputSelection.tsx
+++ audio-test/src/app/InputSelection.tsx
@@ -2,41 +2,82 @@
 
 import { useState, useEffect } from "react";
 interface Stream {
-    data: MediaStream;
-    prevState?: null;
+    data: MediaStream | null;
 };
 
 export default function InputSelection() {
     const [stream, setStream] = useState<Stream | null>(null);
     const [permission, setPermission] = useState(false);
+    const [mediaAccess, setMediaAccess] = useState(false);
+    const [inputDevices, setInputDevices] = useState<MediaDeviceInfo[]>([]);
+    const [selectedInputDeviceId, setSelectedInputDeviceId] = useState<string | null>(null);
 
-    const getInput = async (stream: Stream | null) =>{
-        console.log("getting audio source...")
+    const getMediaAccess = async () => {
         if ("getUserMedia" in navigator.mediaDevices) {
             try {
-                const streamData = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
-                if (stream?.data instanceof MediaStream) {
-                    stream.data = streamData;
-                    console.log("some data", streamData);
-                }
-                console.log("Got audio stream");
+                const streamData = await navigator.mediaDevices.getUserMedia({ audio: true });
                 setPermission(true);
+                setMediaAccess(true);
                 setStream({data: streamData});
             } catch (error) {
                 console.error("Error getting audio stream", error);
+                alert("Failed to get media access.");
             };
         } else {
             alert("Your browser is not supported");
         };
     };
 
+    const useInput = async (stream: Stream | null) =>{
+        try {
+            const streamData = await navigator.mediaDevices.getUserMedia({
+            //Setting audio constraints to use the exact deviceId and if not then leave undefined so the system default is chosen.
+            audio: { deviceId: selectedInputDeviceId ? { exact: selectedInputDeviceId } : undefined },
+            //No video used
+            video: false, 
+            });
+            setPermission(true);
+            setStream({ data: streamData });
+            console.log("Updated Selected Device To: ", streamData)
+        } catch (error) {
+            console.error("Error selecting input device", error);
+            alert("Failed to access microphone.");
+        };
+    };
+
+    // Log stream whenever it changes
     useEffect(() => {
-        console.log("Stream updated:", stream);
-    }, [stream]); // Log stream whenever it changes
+        getMediaAccess();
+        navigator.mediaDevices.enumerateDevices().then(devices => {
+            const audioInputDevices = devices.filter(device => device.kind === "audioinput");
+            setInputDevices(audioInputDevices);
+            console.log('input devices: ', audioInputDevices)
+        }).catch(error => {
+            console.error("Error enumerating devices", error);
+        });
+
+        if (stream !== null) { //Checking if stream isn't null which prevents console from firing twice on initial page render.
+            console.log("Stream updated:", stream);
+        }
+    }, [mediaAccess]); 
+
+    const handleInputDeviceChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setStream(null);
+        setPermission(false);
+        setSelectedInputDeviceId(event.target.value);
+        console.log("Selected:", event.target.value);
+    };
     
     return (
         <div>
-            {!permission ? (<button type="button" onClick={() => getInput(stream)}>Get Audio Source</button>): null}
+            <select id="inputDevice" onChange={handleInputDeviceChange} className="text-black overflow-hidden w-1/6">
+                {inputDevices.map(device => (
+                    <option key={device.deviceId} value={device.deviceId}>
+                        {device.label || `Device ${device.deviceId}`}
+                    </option>
+                ))}
+            </select>
+            {!permission ? (<button type="button" onClick={() => useInput(stream)}>Use Selected Source</button>): null}
             {permission ? (<button type="button">Rec</button>): null}
         </div>
     );
